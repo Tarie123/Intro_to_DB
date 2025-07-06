cefa967346e6a5d9ec1aaf18e58d5520afcef951
Tarie123 <tarymhlang@gmail.com>
2025-07-06 20:50:41 +0200

    fixed database

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -1,5 +1,5 @@
--- Create the database
-CREATE DATABASE alx_book_store;
+-- Create the database if it does not already exist
+CREATE DATABASE IF NOT EXISTS alx_book_store;
 
 -- Use the database
 USE alx_book_store;
@@ -39,9 +39,4 @@ CREATE TABLE Orders (
 -- Table: Order_Details
 CREATE TABLE Order_Details (
     orderdetailid INT AUTO_INCREMENT PRIMARY KEY,
-    order_id INT NOT NULL,
-    book_id INT NOT NULL,
-    quantity DOUBLE NOT NULL,
-    FOREIGN KEY (order_id) REFERENCES Orders(order_id),
-    FOREIGN KEY (book_id) REFERENCES Books(book_id)
-);
+    order_id INT_
